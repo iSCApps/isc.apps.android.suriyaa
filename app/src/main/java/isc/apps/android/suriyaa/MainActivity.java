d1f0feb49af8e6d1a949ce0a7e1eabd1c6f3b4f1
suriyaa <isc.suriyaa@gmail.com>
2015-12-23 16:57:07 +0100

    Update MainActivity.java

--- app/src/main/java/isc/apps/android/suriyaa/MainActivity.java
+++ app/src/main/java/isc/apps/android/suriyaa/MainActivity.java
@@ -15,17 +15,17 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
-        WebView mainWebView = (WebView) findViewById(R.id.webview);
+        
+        WebView mainWebView = (WebView) findViewById(R.id.mainWebView);
         WebSettings webSettings = mainWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-
+        
         mainWebView.setWebViewClient(new MyCustomWebViewClient());
         mainWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
-
+        
         mainWebView.loadUrl("http://bit.ly/Suriyaa");
     }
-
+    
     private class MyCustomWebViewClient extends WebViewClient {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
